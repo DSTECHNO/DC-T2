65379d7248ac2dcd3864fcfdf3e24298b6e7551c
IbrahimEthem-Ispir <ispiribrahimethem@gmail.com>
2025-12-17 15:50:16 +0300

    Update viewer.py

--- TOFAS/viewer.py
+++ TOFAS/viewer.py
@@ -445,7 +445,7 @@ with logo_col2:
 if view_tab == "About":
 
     # TOFAS Building Image
-    st.image("TOFAS/tofas.jpg", caption="Tofaş Türk Otomobil Fabrikası A.Ş.", width=1000)
+    st.image("TOFAS/tofas.jpg", caption="Tofaş Türk Otomobil Fabrikası A.Ş.", width=1200)
 
     st.markdown("""
 ### Facility Information
